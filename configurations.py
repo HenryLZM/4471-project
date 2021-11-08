6531e01200b15cb8ca37fed07a8d11e829f71576
WANG Zeyu <zwangek@connect.ust.hk>
2021-11-08 21:57:10 +0800

    nov8 testing gan

---
+++ configurations.py
@@ -0,0 +1,9 @@
+import h5py
+import torch
+import numpy as np
+
+device = 'cuda'
+
+path_g = './weights/g.pth'
+path_d = './weights/d.pth'
+data_dir = 'E:/HKUST/2021F/COMP4471/project/data/img/G2.h5'
