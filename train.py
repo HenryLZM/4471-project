28e8fd70102be45de1492385560ab1d03403048f
WANG Zeyu <zwangek@connect.ust.hk>
2021-11-09 21:37:25 +0800

    sketch train loop

--- train.py
+++ train.py
@@ -1,9 +1,15 @@
 from data import train_loader, val_loader, test_loader
-from gan_model import Generator,Discriminator
+from gan_model import Generator,Discriminator, weights_init
 from hyperparameters import nz, ngf, ndf
 from hyperparameters import g_lr, d_lr
 import torch
 from tqdm import tqdm
+from configurations import path_g, path_d, device
+from torchvision.transforms import ToPILImage
+from torch.utils.tensorboard import SummaryWriter
+from hed import HedNet
+#tensorboard --logdir=./tensorboard --port 8080
+
 
 real_label = 1.0
 fake_label = 0.0
@@ -11,20 +17,22 @@ fake_label = 0.0
 g = Generator(nz, ngf)
 d = Discriminator(ndf)
 
+cp = torch.load('./checkpoints/iter20800.tar')
+
+g.load_state_dict(cp['g_state_dict'])
+d.load_state_dict(cp['d_state_dict'])
+
 g_opt = torch.optim.Adam(g.parameters(), g_lr)
 d_opt = torch.optim.Adam(d.parameters(), d_lr)
 
-g_sche = torch.optim.lr_scheduler.ReduceLROnPlateau(g_opt)
-d_sche = torch.optim.lr_scheduler.ReduceLROnPlateau(d_opt)
-
 loss_fn = torch.nn.BCELoss()
 
-def train(g, d, g_optimizer, d_optimizer, g_schedular, d_schedular, criterion, train_loader, val_loader, epoch, printevery, device='cuda'):
+def train(g, d, g_optimizer, d_optimizer, criterion, train_loader, val_loader, epoch, printevery, device='cuda'):
     g.to(device)
     d.to(device)
-    d_losses = []
-    g_losses = []
-    iteration = 0
+    writer = SummaryWriter('./tensorboard')
+
+    iteration = cp['iteration']
     for e in range(epoch):
         print(f'Epoch {e+1}')
         # all real
@@ -51,8 +59,7 @@ def train(g, d, g_optimizer, d_optimizer, g_schedular, d_schedular, criterion, t
             loss_d = (loss_d_real + loss_d_fake).item()
             d_optimizer.step()
 
-            # Generator
-            
+            # Generator        
             label.fill_(real_label)
             output = d(fake)
             D_G_z2 = output.mean().item()
@@ -61,13 +68,69 @@ def train(g, d, g_optimizer, d_optimizer, g_schedular, d_schedular, criterion, t
             loss_g.backward(retain_graph=True)
             g_optimizer.step()
             loss_g = loss_g.item()
-
+            if loss_g > 30.0:
+                break
+            writer.add_scalar('generator loss', loss_g, iteration)
+            writer.add_scalar('discriminator loss', loss_d, iteration)
+            if iteration % 100 == 0:
+                ToPILImage()((fake[0]+1)/2).save(f'./output/iteration{iteration}.png')
+            if iteration % 1600 == 0:
+                torch.save({
+                    'epoch': e,
+                    'iteration': iteration,
+                    'g_state_dict': g.state_dict(),
+                    'd_state_dict': d.state_dict(),
+                    'info': f'Iteration {iteration}: LOSS_G={loss_g:.4f}, LOSS_D={loss_d:.4f}, D(x)={D_x:.4f}, D(G(z))={D_G_z1:.4f} / {D_G_z2:.4f}'
+                }, f'./checkpoints/iter{iteration}.tar')
             if i % printevery == 0:
-                tqdm.write(f'Iteration {iteration}: LOSS_G= {loss_g}, LOSS_D= {loss_d}, D(x)= {D_x}, D(G(z))= {D_G_z1} / {D_G_z2}')
-            g_losses.append(loss_g)
-            d_losses.append(loss_d)
-        #g_schedular.step()
-        #d_schedular.step()
-    return g_losses, d_losses
+                tqdm.write(f'Iteration {iteration}: LOSS_G={loss_g:.4f}, LOSS_D={loss_d:.4f}, D(x)={D_x:.4f}, D(G(z))={D_G_z1:.4f} / {D_G_z2:.4f}')
 
-#train(g, d, g_opt, d_opt, g_sche, d_sche, loss_fn, train_loader, val_loader, 1, 10)
+        # g_schedular.step(loss_g)
+        # d_schedular.step(loss_d)
+    torch.save(g.state_dict(), './weights/g.pth')
+    torch.save(d.state_dict(), './weights/d.pth')
+
+
+hed = HedNet().to(device)
+d_sketch = Discriminator(ndf).to(device)
+def train_with_sketch(g, d_sketch, sketches, g_opt, d_opt, num, epoch, silence, device='cuda', printevery=10):
+    g.to(device)
+    for e in range(epoch):
+        noise = torch.randn(num, nz, ngf, device=device)
+        fake = g(noise)
+        fake_sketches = hed(fake)
+
+        # all real
+        label = torch.full(size=(N,), fill_value=real_label, device=device)
+        if e % silence != 0:
+            D_x = d_sketch(sketches)
+            loss_d_real = loss_fn(D_x, label)
+
+            # all fake
+            label.fill_(fake_label)
+            D_G_z1 = d_sketch(fake_sketches)
+            loss_d_fake = loss_fn(D_G_z1, label)
+
+            loss_d = loss_d_fake + loss_d_real
+            d_opt.zero_grad()
+            loss_d.backward()
+            d_opt.step()
+
+        # generator
+        label.fill_(real_label)
+        D_G_z2 = d_sketch(fake_sketches)
+        loss_g = loss_fn(D_G_z2, label)
+        g_opt.zero_grad()
+        loss_g.backward()
+        g_opt.step()
+
+        loss_g = loss_g.item()
+        loss_d = loss_d.item()
+        D_x = D_x.item()
+        D_G_z1 = D_G_z1.item()
+        D_G_z2 = D_G_z2.item()
+
+        if i % printevery == 0:
+            print(f'Epoch {e+1}: LOSS_G={loss_g:.4f}, LOSS_D={loss_d:.4f}, D(x)={D_x:.4f}, D(G(z))={D_G_z1:.4f} / {D_G_z2:.4f}')
+
+    torch.save(g.state_dict(), './weights/g_modified.pth')
