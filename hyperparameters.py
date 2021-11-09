8f42e7c576a210a43f317647412d15e98b89689a
WANG Zeyu <zwangek@connect.ust.hk>
2021-11-10 00:11:12 +0800

    removing submodule

--- hyperparameters.py
+++ hyperparameters.py
@@ -1,5 +1,5 @@
 # DATA
-batch_size = 256
+batch_size = 32
 
 # MODEL
 nz = 100
@@ -8,6 +8,13 @@ ndf = 64
 
 # TRAIN
 #   Generator
-g_lr = 1e-3
-#   Discreminator
-d_lr = 1e-3
+g_lr = 5e-4
+g_beta1, g_beta2 = 0.5, 0.999
+#   Fashion Discreminator
+d_lr = 4e-6
+d_beta1, d_beta2 = 0.5, 0.999
+#   Sketch Disciminator
+sg_lr = 1e-4
+sg_beta1, sg_beta2 = 0.5, 0.999
+sd_lr = 1e-6
+sd_beta1, sd_beta2 = 0.5, 0.999
