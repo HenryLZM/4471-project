bdfe4b7c7a31f19e26582a759fb4b28223131c19
WANG Zeyu <zwangek@connect.ust.hk>
2021-11-10 14:25:01 +0800

    update

--- configurations.py
+++ configurations.py
@@ -1,9 +1,27 @@
-import h5py
 import torch
-import numpy as np
 
-device = 'cuda'
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 path_g = './weights/g.pth'
 path_d = './weights/d.pth'
 data_dir = 'E:/HKUST/2021F/COMP4471/project/data/img/G2.h5'
+
+generated_dir = './test/'
+
+# photo_sketch_dir = './PhotoSketch/'
+# photo_sketch_cmd = f'python {photo_sketch_dir}test_pretrained.py ^ \
+#     --name pretrained ^ \
+#     --dataset_mode test_dir ^   \
+#     --dataroot {generated_dir} ^  \
+#     --results_dir ./sketch/ ^  \
+#     --checkpoints_dir  {photo_sketch_dir}checkpoints/^  \
+#     --model pix2pix ^   \
+#     --which_direction AtoB ^    \
+#     --norm batch ^  \
+#     --input_nc 3 ^  \
+#     --output_nc 1 ^ \
+#     --which_model_netG resnet_9blocks ^ \
+#     --no_dropout ^'
+
+
+hed_weight = './weights/hed-bsds500'
--- data.py
+++ data.py
@@ -2,18 +2,22 @@ from torch.utils.data import Dataset, DataLoader
 from hyperparameters import batch_size
 import h5py
 import torch
+from torchvision.transforms import Normalize, ToPILImage
+import numpy as np
+from configurations import data_dir
 
 class FashionDataset(Dataset):
     def __init__(self, dir, purpose, device):
         f = h5py.File(dir)
+        self.masks = f['b_']
         self.images = f['ih']
-        self.mean = f['ih_mean']
+        self.mean = np.array(f['ih_mean'])
         self.purpose = purpose
         self.device = device
 
     def __len__(self):
         if self.purpose == 'train':
-            return 60000
+            return 2048
         elif self.purpose == 'val':
             return 10000
         elif self.purpose == 'test':
@@ -28,10 +32,12 @@ class FashionDataset(Dataset):
             base = 70000
 
         pic = self.images[base+idx] + self.mean
-        pic = pic.clip(0,1)
-        return torch.tensor(pic, device=self.device)
+        pic = pic.clip(0,1) * ((self.masks[base+idx] != 0))
+        pic = torch.tensor(pic, device=self.device)
+        pic = Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))(pic)
+        return pic
 
-directory = 'E:/HKUST/2021F/COMP4471/project/data/img/G2.h5'
+directory = data_dir
 train_set = FashionDataset(directory, 'train', 'cuda')
 val_set = FashionDataset(directory, 'val', 'cuda')
 test_set = FashionDataset(directory, 'test', 'cuda')
